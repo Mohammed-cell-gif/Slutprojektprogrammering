ca614dd1a94b7f06327ea146d5fdd2b36555032f
Mohammed-cell-gif <moal08008@utb.vaxjo.se>
2026-04-10 13:17:43 +0200

    Början av text baserad spel
    Jag började med att göra en klass för fiender och att man trycker enter för att det ska fungera.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+Console.WriteLine("Press Enter to start");
+if(string.IsNullOrEmpty(Console.ReadLine()))
+{
+    bool start=true;
+}
+Ememies f = new Ememies();
+
+class Ememies
+{
+    private string name;
+    private string power;
+    private int HP;
+    private string Weakness;
+
+}
+
+    
